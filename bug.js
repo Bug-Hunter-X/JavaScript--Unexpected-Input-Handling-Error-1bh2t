5d078f47beb5bcbac9dd81b2b2c6d27043fd62a4
BugHunter-X <askzenai@gmail.com>
2025-02-19 18:12:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+function foo(x) {
+  if (x === null) {
+    return 0; // Handle null case
+  } else if (typeof x !== 'number') {
+    throw new Error('Invalid input: x must be a number.');
+  }
+
+  return x + 1;
+}
+
+console.log(foo(5)); // Output: 6
+console.log(foo(null)); // Output: 0
+console.log(foo('hello')); // Throws an error
